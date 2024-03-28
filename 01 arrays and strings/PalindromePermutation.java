ee047050d056ca09bd98d57a4b48c8e519e448ee
Tony Li <tony.lilui416@gmail.com>
2024-03-27 21:18:45 -0400

    finished palindrome permutation

--- 01 arrays and strings/PalindromePermutation.java
+++ 01 arrays and strings/PalindromePermutation.java
@@ -1,5 +1,29 @@
 public class PalindromePermutation {
-    public static void main(String[] args) {
+    public static boolean palindromePermutation(String str) {
+        str = str.replace(" ", "");
+        String sorted = sort(str);
+        int doubleCount = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            char current = sorted.charAt(i);
+            char next = sorted.charAt(i + 1);
+            if (current == next) {
+                doubleCount++;
+            }
+        }
+        if (doubleCount == str.length() / 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    public static String sort(String str) {
+        char[] strChars = str.toCharArray();
+        java.util.Arrays.sort(strChars);
+        return new String(strChars);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindromePermutation("tact coa"));
     }
 }
