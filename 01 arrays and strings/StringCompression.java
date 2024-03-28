1eae93f0b5dc4fed9f7b960fa748ee24fc11e83a
Tony Li <tony.lilui416@gmail.com>
2024-03-28 11:04:44 -0400

    finished string compression

--- 01 arrays and strings/StringCompression.java
+++ 01 arrays and strings/StringCompression.java
@@ -1,5 +1,27 @@
 public class StringCompression {
-    public static void main(String[] args) {
+    public static String stringCompression(String str) {
+        char current = str.charAt(0);
+        int counter = 1;
+        String result = "";
+        for (int i = 0; i < str.length() - 1; i++) {
+            char next = str.charAt(i + 1);
+            if (current == next) {
+                counter++;
+            } else {
+                result += current;
+                result += String.valueOf(counter);
+                counter = 1;
+            }
+            current = next;
+        }
+        result += current;
+        result += String.valueOf(counter);
+        counter = 1;
 
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringCompression("aaaabccc"));
     }
 }
