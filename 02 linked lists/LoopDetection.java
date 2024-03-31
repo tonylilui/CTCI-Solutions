a30a8f3f7206be0dd3370c6547c9f961aca347ac
Tony Li <tony.lilui416@gmail.com>
2024-03-31 19:42:04 -0400

    finished loop detection

--- 02 linked lists/LoopDetection.java
+++ 02 linked lists/LoopDetection.java
@@ -1,3 +1,37 @@
-public class LoopDetection {
+import java.util.HashSet;;
 
+public class LoopDetection {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node loopDetection(Node head) {
+        HashSet<Node> set = new HashSet<Node>();
+        while (head != null) {
+            if (head.next != null && set.contains(head.next)) {
+                return head;
+            } else {
+                set.add(head);
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+        System.out.println(loopDetection(node1).data);
+    }
 }
